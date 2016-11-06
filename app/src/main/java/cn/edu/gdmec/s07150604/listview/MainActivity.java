f53f4e4af7eb479c0403f55446e0b905639c1934
gdmec07150604 <1552742415@qq.com>
2016-11-06 22:21:48 +0800

    Initial commit

---
+++ app/src/main/java/cn/edu/gdmec/s07150604/listview/MainActivity.java
@@ -0,0 +1,197 @@
+package cn.edu.gdmec.s07150604.listview;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.provider.ContactsContract;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Layout;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+    private ListView lv;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        lv=(ListView) findViewById(R.id.listView);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        menu.add(0,1,0,"ArrayAdapter");
+        menu.add(0,2,0,"SimpleCursorAdapter");
+        menu.add(0,3,0,"SinpleAdapter");
+        menu.add(0,4,0,"BaseAdapter");
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case 1:
+                arrayAdapter();
+                break;
+            case 2:
+                simpleCursorAdapter();
+                break;
+            case 3:
+                simpleAdapter();
+                break;
+            case 4:
+                baseAdapter();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    public void  arrayAdapter(){
+        final String [] weeks ={"Sunday","Monday","Tuesday","Wen","Thus","fri","sat"};
+        ArrayAdapter  arrayAdapter =new ArrayAdapter(this,android.R.layout.simple_list_item_1,weeks);
+        lv.setAdapter(arrayAdapter);
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this,weeks[position],Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+    public void simpleCursorAdapter(){
+        final Cursor mCursor =getContentResolver().query(ContactsContract.Contacts.CONTENT_URI,null,null,null,null);
+        startManagingCursor(mCursor);
+        SimpleCursorAdapter mAdapter =new SimpleCursorAdapter(this,android.R.layout.simple_expandable_list_item_1
+                ,mCursor,new String []{ ContactsContract.Contacts.DISPLAY_NAME},
+                new int[]{android.R.id.text1});
+        lv.setAdapter(mAdapter);
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this,mCursor.getString(mCursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME)) ,
+                        Toast.LENGTH_SHORT).show();
+
+            }
+        });
+    }
+    public void simpleAdapter(){
+        final List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
+        Map<String, Object> map =new HashMap<String, Object >();
+        map.put("title","G1");
+        map.put("info","Google 1");
+        map.put("img",R.drawable.icon1);
+        list.add(map);
+        Map<String, Object> map1 =new HashMap<String, Object >();
+        map.put("title","G2");
+        map.put("info","Google 2");
+        map.put("img",R.drawable.icon2);
+        list.add(map1);
+
+        SimpleAdapter mAdapter = new SimpleAdapter(this, list,R.layout.simpleadapter_demo,
+                new String[]{"img","title","info"},
+                new int[]{R.id.imageView,R.id.infoView}
+        );
+        lv.setAdapter(mAdapter);
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            public void onItemClick(AdapterView<?> parent,View view,int position,long id){
+                Toast.makeText(MainActivity.this,list.get(position).get("title").toString(),Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+    static class ViewHolder
+    {
+        public ImageView img;
+        public TextView title;
+        public TextView info;
+        public Button btn;
+        public View layou;
+    }
+    public  void baseAdapter(){
+        class MyBaseAdapter extends BaseAdapter{
+            private List<Map<String,Object>> data;
+            private Context context;
+            private LayoutInflater myLayoutInflater;
+
+            public MyBaseAdapter(Context context,List<Map<String,Object>> data){
+                super();
+                this.data=data;
+                this.context =context;
+                this.myLayoutInflater =LayoutInflater.from(context);
+            }
+
+            @Override
+            public int getCount() {
+                return data.size();
+            }
+
+            @Override
+            public Object getItem(int position) {
+                return data.get(position);
+            }
+
+            @Override
+            public long getItemId(int position) {
+                return position;
+            }
+
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                ViewHolder holder =null;
+                if(convertView==null){
+                    holder =new ViewHolder();
+                    convertView=myLayoutInflater.inflate(R.layout.l1,parent,false);
+                    holder.img=(ImageView) convertView.findViewById(R.id.img);
+                    holder.title=(TextView) convertView.findViewById(R.id.title);
+                    holder.info=(TextView) convertView.findViewById(R.id.info);
+                    holder.btn=(Button)convertView.findViewById(R.id.btn);
+                    holder.layou=convertView.findViewById(R.id.l1);
+                    convertView.setTag(holder);
+                }else{
+                    holder=(ViewHolder) convertView.getTag();
+                }
+                holder.img.setImageResource(Integer.parseInt(data.get(position).get("img").toString()));
+                holder.title.setText(data.get(position).get("title").toString());
+                holder.info.setText(data.get(position).get("info").toString());
+                if(position%2==1){
+                    holder.layou.setBackgroundColor(ContextCompat.getColor(context,R.color.colorAccent));
+                }else{
+                    holder.layou.setBackgroundColor(ContextCompat.getColor(context,R.color.colorPrimary));
+                }
+
+                return convertView;
+            }
+        }
+        final List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
+        Map<String, Object> map =new HashMap<String, Object >();
+        map.put("title","G1");
+        map.put("info","Google 1");
+        map.put("img",R.drawable.icon1);
+        list.add(map);
+
+
+        MyBaseAdapter myBaseAdapter =new MyBaseAdapter(this,list);
+        lv.setAdapter(myBaseAdapter);
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            public void onItemClick(AdapterView<?> parent,View view,int position,long id){
+                Toast.makeText(MainActivity.this,list.get(position).get("title").toString(),Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
